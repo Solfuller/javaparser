f03c0c5e0db16452384ebd113ac82c4032a19b61
Solomon Fuller <fullersl17@gcc.edu>
2021-03-06 16:35:10 -0500

    Added setup test case

---
+++ javaparser-core-testing/src/test/java/com/github/javaparser/SetupTest.java
@@ -0,0 +1,19 @@
+/**
+ * Obtained from:
+ * https://github.com/javaparser/javaparser/wiki/Eclipse-Project-Setup-Guide
+ */
+
+package com.github.javaparser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SetupTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
